aa6e1d2953ee59eab5efb0f9a8dd7cf86e02826a
shekar007 <shekar2003hfs@gmail.com>
2023-04-20 15:50:22 -0700

    code changed

--- src/imu_publisher/src/publisher.py
+++ src/imu_publisher/src/publisher.py
@@ -3,28 +3,30 @@
 import rospy
 from sensor_msgs.msg import Imu
 from geometry_msgs.msg import Vector3
+import math
 
 def imu_publisher():
     rospy.init_node('imu_publisher', anonymous=True)
     imu_pub = rospy.Publisher('/imu/data_raw', Imu, queue_size=10)
-    rate = rospy.Rate(10) # 10hz
+    rate = rospy.Rate(100) # 100hz
 
     imu_msg = Imu()
     imu_msg.header.frame_id = 'imu_link'
     imu_msg.orientation_covariance[0] = -1
 
-    lin_acc = Vector3()
-    lin_acc.x = 2.5
-    lin_acc.y = 0
-    lin_acc.z = 9.8
+    for i in range(1000):
+        t = float(i)/rate.to_sec()
+        ang_vel = Vector3()
+        ang_vel.x = math.sin(t)
+        ang_vel.y = math.cos(t)
+        ang_vel.z = math.sin(2*t)
 
-    for i in range(100):
-        ang_acc = Vector3()
-        ang_acc.x = 0
-        ang_acc.y = 0
-        ang_acc.z = i/10.0
+        lin_acc = Vector3()
+        lin_acc.x = math.sin(3*t)
+        lin_acc.y = math.cos(2*t)
+        lin_acc.z = math.sin(t)
 
-        imu_msg.angular_velocity = ang_acc
+        imu_msg.angular_velocity = ang_vel
         imu_msg.linear_acceleration = lin_acc
 
         imu_pub.publish(imu_msg)
